4b190a6c31e0d1002a53935bf158dd9280d927f0
Jayabharathi2210 <22csec11@gmail.com>
2025-06-18 21:23:47 +0530

    first commit

---
+++ Booking.java
@@ -0,0 +1,17 @@
+public class Booking {
+
+    int bookingId, customerId, pickupTime, dropTime, amount;
+    char from, to;
+
+    public Booking()
+    {
+        this.bookingId = bookingId;
+        this.customerId = customerId;
+        this.pickupTime = pickupTime;
+        this.dropTime = dropTime;
+        this.amount = amount;
+        this.from = from;
+        this.to = to;
+    }
+    
+}
